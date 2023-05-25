b09dcb71d5ef55d808fee68b56f6bf0115215f85
Vishwas R Nayak <vishwasrnayak@outlook.com>
2023-05-25 16:30:14 +0530

    New scrip added

---
+++ BankNUnitTest/BankAccountTest1.cs
@@ -0,0 +1,47 @@
+using Bank;
+
+namespace BankNUnitTest
+{
+    public class BankAccountTest1
+    {
+        [Test]
+        public void Adding_Funds_Updates_Balance()
+        {
+            //ARRANGE
+            var account = new BankAccount(1000);
+
+            //ACT
+            account.Add(500);
+
+            //ASSERT
+            Assert.AreEqual(1500, account.Balance);
+        }
+
+        [Test]
+        public void Withdrawing_Funds_Updates_Balance()
+        {
+            //ARRANGE
+            var account = new BankAccount(1000);
+
+            //ACT
+            account.Withdraw(500);
+
+            //ASSERT
+            Assert.AreEqual(500, account.Balance);
+        }
+        [Test]
+        public void Transfering_Funds_Updates_Both_Accounts()
+        {
+            //ARRANGE
+            var account = new BankAccount(1000);
+            var otherAccount = new BankAccount();
+
+            //ACT
+            account.TransferFundsTo(otherAccount, 500);
+
+            //ASSERT
+            Assert.AreEqual(500, account.Balance);
+            Assert.AreEqual(500, otherAccount.Balance);
+        }
+    }
+}
